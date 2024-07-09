12530ebf158f04c0adf78552842427f4d834fc92
shabbeer <mdshabbeer707@gmail.com>
2024-07-09 18:47:05 +0530

    tags added

--- src/App.js
+++ src/App.js
@@ -17,34 +17,74 @@ import Industry from './Pages/Industry/Industry';
 import Clients from './Pages/Clients/Clients';
 import Contact from './Pages/Contact/Contact';
 import LanguageCard from './Components/LanguageCard/LanguageCard';
-
+import Host from './Pages/Host';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(`${Host}/api/meta/fetchallmeta`, {
+          headers: {
+            'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjY3NmEzNDQ1NzQyZjM1NjgyZTNlMWNjIn0sImlhdCI6MTcxOTA1MTA4NH0.OsZKI_I3GuMyljUYJmdqTCSxFWy_BPaNhDb2gfnXb6Q',
+            'Content-Type': 'application/json',
+          },
+        });
+        setApiData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const formatPathname = (pathname) => {
+    return pathname?.toLowerCase().replace(/\s+/g, '-');
+  };
+
+  const getMeta = (path) => {
+    if (!apiData) return { title: "", descriptions: "" };
+    const formattedPath = formatPathname(path);
+    const meta = apiData.find(item => formatPathname(item.page) === formattedPath);
+    return meta && meta.subcategories.length > 0
+      ? { title: meta.subcategories[0].title, descriptions: meta.subcategories[0].description }
+      : { title: "", descriptions: "" };
+  };
+  console.log(getMeta('home'), "meta")
+
   return (
     <>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/about" exact element={<About />} />
-            <Route path="/translation-services" exact element={<Translation />} />
-            <Route path="/interpretation-services" exact element={<Interpretation />} />
-            <Route path="/transcription-services" exact element={<Transcription />} />
-            <Route path="/apostille-services" exact element={<Apostille />} />
-            <Route path="/simultaneous-equipment" exact element={<Simultaneous />} />
-            <Route path="/european-languages" exact element={<Languages heading={"European"} />} />
-            <Route path="/asian-languages" exact element={<Languages heading={"Asian"} />} />
-            <Route path="/indian-languages" exact element={<Languages heading={"Indian"} />} />
-            <Route path="/:pathName" exact element={<LanguageCard />} />
-            <Route path="/services" exact element={<Services />} />
-            <Route path="/industry" exact element={<Industry />} />
-            <Route path="/clients" exact element={<Clients />} />
-            <Route path="/contact" exact element={<Contact />} />
-            <Route path="/blogs/" exact element={<Blogs />} />
-            <Route path="/blogs/:pathName" exact element={<BlogDetails />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home {...getMeta('home')} />} />
+          <Route path="/about" exact element={<About {...getMeta('about')} />} />
+          <Route path="/translation-services" exact element={<Translation {...getMeta('translation-services')} />} />
+          <Route path="/interpretation-services" exact element={<Interpretation {...getMeta('interpretation-services')} />} />
+          <Route path="/transcription-services" exact element={<Transcription {...getMeta('transcription-services')} />} />
+          <Route path="/apostille-services" exact element={<Apostille {...getMeta('apostille-services')} />} />
+          <Route path="/simultaneous-equipment" exact element={<Simultaneous {...getMeta('simultaneous-equipment')} />} />
+          <Route path="/european-languages" exact element={<Languages heading={"European"} {...getMeta('european-languages')} />} />
+          <Route path="/asian-languages" exact element={<Languages heading={"Asian"} {...getMeta('asian-languages')} />} />
+          <Route path="/indian-languages" exact element={<Languages heading={"Indian"} {...getMeta('indian-languages')} />} />
+          <Route path="/:pathName" exact element={<LanguageCard {...getMeta(window.location.pathname.slice(1))} />} />
+          <Route path="/services" exact element={<Services {...getMeta('services')} />} />
+          <Route path="/industry" exact element={<Industry {...getMeta('industry')} />} />
+          <Route path="/clients" exact element={<Clients {...getMeta('clients')} />} />
+          <Route path="/contact" exact element={<Contact {...getMeta('contact')} />} />
+          <Route path="/blogs" exact element={<Blogs {...getMeta('blogs')} />} />
+          <Route path="/blogs/:pathName" exact element={<BlogDetails {...getMeta(window.location.pathname.slice(7))} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./About.css"
 import aboutimg from "../../Assets/about/person.png"
+import { useLocation } from 'react-router-dom';
 
-const About = () => {
+const About = (props) => {
+
+  /* global dataLayer */
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = props.title;
+
+    const canonicalUrl = `${window.location.origin}${location.pathname}`;
+    let canonicalLink = document.querySelector("link[rel='canonical']");
+    if (canonicalLink) {
+      canonicalLink.setAttribute("href", canonicalUrl);
+    } else {
+      canonicalLink = document.createElement("link");
+      canonicalLink.setAttribute("rel", "canonical");
+      canonicalLink.setAttribute("href", canonicalUrl);
+      document.head.appendChild(canonicalLink);
+    }
+
+    const description = props.descriptions;
+    let metaDescription = document.querySelector("meta[name='description']");
+    if (metaDescription) {
+      metaDescription.setAttribute("content", description);
+    } else {
+      metaDescription = document.createElement("meta");
+      metaDescription.setAttribute("name", "description");
+      metaDescription.setAttribute("content", description);
+      document.head.appendChild(metaDescription);
+    }
+
+    // Ensure dataLayer is initialized before the GA script loads
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag() {
+    //   dataLayer.push(arguments);
+    // }
+
+    // Load the Google Analytics script only once
+    // const gaScriptId = 'ga-gtag';
+    // if (!document.getElementById(gaScriptId)) {
+    //   const script = document.createElement('script');
+    //   script.id = gaScriptId;
+    //   script.async = true;
+    //   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-3BK9F87D6E';
+    //   document.head.appendChild(script);
+
+    //   script.onload = () => {
+    //     gtag('js', new Date());
+    //     gtag('config', 'G-3BK9F87D6E');
+    //   };
+    // }
+  }, [props.title, props.descriptions, location.pathname]);
+
   return (
     <div className='about'>
           <img src="https://static.vecteezy.com/system/resources/previews/027/187/311/large_2x/arab-man-confidently-points-towards-copy-space-on-yellow-background-for-designers-to-add-text-or-logos-free-photo.jpg" alt="" />
