2301168d51a3533b0cfdc535f22bdab27793b567
shabbeer <mdshabbeer707@gmail.com>
2024-10-05 16:20:08 +0530

    new update of blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,62 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import "./Blogs.css"
+import React, { useEffect, useState } from 'react';
+import './Blogs.css';
 import { Link, useLocation } from 'react-router-dom';
-import BlogData from '../../Components/Blog/BlogData';
 import axios from 'axios';
 import Host from '../Host';
 
 const Blogs = (props) => {
+    const location = useLocation();
 
-     /* global dataLayer */
-  const location = useLocation();
+    useEffect(() => {
+        document.title = props.title;
 
-  useEffect(() => {
-    document.title = props.title;
+        const canonicalUrl = `${window.location.origin}${location.pathname}`;
+        let canonicalLink = document.querySelector("link[rel='canonical']");
+        if (canonicalLink) {
+            canonicalLink.setAttribute("href", canonicalUrl);
+        } else {
+            canonicalLink = document.createElement("link");
+            canonicalLink.setAttribute("rel", "canonical");
+            canonicalLink.setAttribute("href", canonicalUrl);
+            document.head.appendChild(canonicalLink);
+        }
 
-    const canonicalUrl = `${window.location.origin}${location.pathname}`;
-    let canonicalLink = document.querySelector("link[rel='canonical']");
-    if (canonicalLink) {
-      canonicalLink.setAttribute("href", canonicalUrl);
-    } else {
-      canonicalLink = document.createElement("link");
-      canonicalLink.setAttribute("rel", "canonical");
-      canonicalLink.setAttribute("href", canonicalUrl);
-      document.head.appendChild(canonicalLink);
-    }
-
-    const description = props.descriptions;
-    let metaDescription = document.querySelector("meta[name='description']");
-    if (metaDescription) {
-      metaDescription.setAttribute("content", description);
-    } else {
-      metaDescription = document.createElement("meta");
-      metaDescription.setAttribute("name", "description");
-      metaDescription.setAttribute("content", description);
-      document.head.appendChild(metaDescription);
-    }
-
-    // Ensure dataLayer is initialized before the GA script loads
-    // window.dataLayer = window.dataLayer || [];
-    // function gtag() {
-    //   dataLayer.push(arguments);
-    // }
-
-    // Load the Google Analytics script only once
-    // const gaScriptId = 'ga-gtag';
-    // if (!document.getElementById(gaScriptId)) {
-    //   const script = document.createElement('script');
-    //   script.id = gaScriptId;
-    //   script.async = true;
-    //   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-3BK9F87D6E';
-    //   document.head.appendChild(script);
-
-    //   script.onload = () => {
-    //     gtag('js', new Date());
-    //     gtag('config', 'G-3BK9F87D6E');
-    //   };
-    // }
-  }, [props.title, props.descriptions, location.pathname]);
-
+        const description = props.descriptions;
+        let metaDescription = document.querySelector("meta[name='description']");
+        if (metaDescription) {
+            metaDescription.setAttribute("content", description);
+        } else {
+            metaDescription = document.createElement("meta");
+            metaDescription.setAttribute("name", "description");
+            metaDescription.setAttribute("content", description);
+            document.head.appendChild(metaDescription);
+        }
+    }, [props.title, props.descriptions, location.pathname]);
 
     const scrollToTop = () => {
         window.scrollTo({
@@ -64,8 +39,6 @@ const Blogs = (props) => {
             behavior: 'auto'
         });
     };
-
-    // api calling
 
     const [apiData, setApiData] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -94,6 +67,21 @@ const Blogs = (props) => {
         return pathname.toLowerCase().replace(/\s+/g, '-');
     };
 
+    // Filter blogs based on publishDate and status
+    const filteredBlogs = apiData?.filter(blog => {
+        const publishDate = new Date(blog.publishDate);
+        const currentDate = new Date();
+        const currentDateUTC = new Date(
+            publishDate.getUTCFullYear(),
+            publishDate.getUTCMonth(),
+            publishDate.getUTCDate(),
+            publishDate.getUTCHours(),
+            publishDate.getUTCMinutes(),
+            publishDate.getUTCSeconds()
+        );
+        return blog.status === true && blog.draft === false && currentDateUTC <= currentDate;
+    });
+
     return (
         <div className='blog'>
             {loading ? (
@@ -118,26 +106,25 @@ const Blogs = (props) => {
                                 <div className="blog-body-left">
                                     <div className="blog-body-image">
                                         <Link to={{
-                                            pathname: `/blogs/${formatPathname(apiData[apiData?.length - 1].tag)}/`
-                                        }} onClick={scrollToTop} >
-                                            <img src={apiData[apiData?.length - 1].catimageUrl} alt="" />
-                                            {/* <img src={apiData[apiData?.length - 1].catimageUrl} alt={apiData[apiData?.length - 1].category} /> */}
+                                            pathname: `/blogs/${formatPathname(filteredBlogs[filteredBlogs?.length - 1]?.tag)}/`
+                                        }} onClick={scrollToTop}>
+                                            <img src={filteredBlogs[filteredBlogs?.length - 1]?.catimageUrl} alt="" />
                                         </Link>
                                     </div>
                                     <div className="blog-body-detail">
-                                        <h3>{apiData[apiData?.length - 1].category}</h3>
-                                        <h6>{new Date(apiData[apiData?.length - 1].date).toLocaleDateString()}</h6>
-                                        <p>{apiData[apiData?.length - 1].categorydesc}</p>
+                                        <h3>{filteredBlogs[filteredBlogs?.length - 1]?.category}</h3>
+                                        <h6>{new Date(filteredBlogs[filteredBlogs?.length - 1]?.date).toLocaleDateString()}</h6>
+                                        <p>{filteredBlogs[filteredBlogs?.length - 1]?.categorydesc}</p>
                                         <div className="body-left-button">
                                             <Link to={{
-                                                pathname: `/blogs/${formatPathname(BlogData[BlogData.length - 1].pathName)}/`
+                                                pathname: `/blogs/${formatPathname(filteredBlogs[filteredBlogs?.length - 1]?.tag)}/`
                                             }} onClick={scrollToTop}>View More</Link>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="blog-body-right">
                                     <div className="blog-right-items">
-                                        {apiData.slice().reverse().map((item) => (
+                                        {filteredBlogs.slice().reverse().map((item) => (
                                             <div className="blogs-items-card" key={item.tag}>
                                                 <Link to={{
                                                     pathname: `/blogs/${formatPathname(item.tag)}/`
@@ -161,4 +148,4 @@ const Blogs = (props) => {
     )
 }
 
-export default Blogs
+export default Blogs;
